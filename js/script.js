843c7fe0fd85e18c0e35b25262963006b7e7172c
KOVU9529 <bapgiordano@gmail.com>
2022-06-23 14:27:02 +0200

    completo + bonus

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var app=new Vue(
+    {
+        //Riferimento principale
+        el:'#root',
+        //Creo altri oggetti
+        data:{
+            message:'Hello Vue, this is my first job with you',
+            image:'https://www.greenme.it/wp-content/uploads/2021/03/orione_costellazione.jpg'
+        }
+
+    }
+)
